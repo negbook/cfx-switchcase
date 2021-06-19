e6f08bc347763325c52b225b2b02e256752adfdb
negbook <negbook@gmail.com>
2021-06-20 01:16:03 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,6 +1,5 @@
-local case = {}
+local case = {} --cfx-switchcase by negbook https://github.com/negbook/cfx-switchcase/blob/main/cfx-switchcase.lua
 local switch = setmetatable({},{__call=function(a,b)case=setmetatable({},{__call=function(a,b)return a[b]end,__index=function(a,c)return c and c==b and setmetatable({},{__call=function(a,d)d()end})or function()end end})return a[b]end,__index=function(a,c)return setmetatable({},{__call=function(a,...)end})end})
-
 
 CreateThread(function()
     while true do Wait(0) 
