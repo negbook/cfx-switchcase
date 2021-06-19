cb55e37bcbe1221367506657afe3dc7bfa298139
negbook <negbook@gmail.com>
2021-06-20 00:53:54 +0800

    Update cfx-switchcase.lua

--- cfx-switchcase.lua
+++ cfx-switchcase.lua
@@ -1,3 +1,3 @@
-local case = {}
+local case = {} --cfx-switchcase by negbook
 local switch = setmetatable({},{__call=function(a,b)case=setmetatable({},{__call=function(a,b)return a[b]end,__index=function(a,c)return c and c==b and setmetatable({},{__call=function(a,d)d()end})or function()end end})return a[b]end,__index=function(a,c)return setmetatable({},{__call=function(a,...)end})end})
 
