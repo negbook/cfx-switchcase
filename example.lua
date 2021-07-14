8b7e19e606b4f8bbfc9cef4ed1168b94a7863e4f
negbook <negbook@gmail.com>
2021-07-14 22:22:09 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,6 +1,6 @@
 local case = {} --cfx-switchcase by negbook https://github.com/negbook/cfx-switchcase/blob/main/cfx-switchcase.lua
 local default = {}
-local switch = setmetatable({},{__call=function(a,b)case=setmetatable({},{__call=function(a,b)return a[b]end,__index=function(a,c)return c and c==b and setmetatable({},{__call=function(a,d)default=setmetatable({},{__call=function(a,e)end})d()end})or function()end end})default=setmetatable({},{__call=function(a,b)if b and type(b)=='function'then b()end end})return a[b]end,__index=function(a,c)return setmetatable({},{__call=function(a,...)end})end})
+local switch = setmetatable({},{__call=function(a,b)case=setmetatable({},{__call=function(a,...)return a[{...}]end,__index=function(a,c)local d=false;if c and type(c)=="table"then for e=1,#c do local f=c[e]if f and b and f==b then d=true;break end end end;if d then return setmetatable({},{__call=function(a,g)default=setmetatable({},{__call=function(a,h)end})g()end})else return function()end end end})default=setmetatable({},{__call=function(a,b)if b and type(b)=="function"then b()end end})return a[b]end,__index=function(a,f)return setmetatable({},{__call=function(a,...)end})end})
 
 
 CreateThread(function()
@@ -13,11 +13,8 @@ CreateThread(function()
         
         
         switch(a)(
-            case(1)(function()
-                    print(99)
-            end),
-            case(4)(function()
-                    print(98)
+            case(1,4)(function()
+                print(99)
             end),
             default(function()
                 print(9091)
