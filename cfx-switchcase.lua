63050f7208705570da79139e9a6dbcfb5e13f18d
negbook <negbook@gmail.com>
2021-07-14 22:30:15 +0800

    Update cfx-switchcase.lua

--- cfx-switchcase.lua
+++ cfx-switchcase.lua
@@ -1,4 +1,4 @@
 local case = {} --cfx-switchcase by negbook https://github.com/negbook/cfx-switchcase/blob/main/cfx-switchcase.lua
-local default = {}
+local default = {} --default must put after cases when use
 local switch = setmetatable({},{__call=function(a,b)case=setmetatable({},{__call=function(a,...)return a[{...}]end,__index=function(a,c)local d=false;if c and type(c)=="table"then for e=1,#c do local f=c[e]if f and b and f==b then d=true;break end end end;if d then return setmetatable({},{__call=function(a,g)default=setmetatable({},{__call=function(a,h)end})g()end})else return function()end end end})default=setmetatable({},{__call=function(a,b)if b and type(b)=="function"then b()end end})return a[b]end,__index=function(a,f)return setmetatable({},{__call=function(a,...)end})end})
 
