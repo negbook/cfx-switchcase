082f07a3d0997de4d002a6f5887beff4f1ad306d
negbook <negbook@gmail.com>
2022-02-06 00:12:44 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,8 +1,9 @@
 local case = {} --cfx-switchcase by negbook https://github.com/negbook/cfx-switchcase/blob/main/cfx-switchcase.lua
 local default = {} --default must put after cases when use
 local switch = setmetatable({},{__call=function(a,b)case=setmetatable({},{__call=function(a,...)return a[{...}]end,__index=function(a,c)local d=false;if c and type(c)=="table"then for e=1,#c do local f=c[e]if f and b and f==b then d=true;break end end end;if d then return setmetatable({},{__call=function(a,g)default=setmetatable({},{__call=function(a,h)end})g()end})else return function()end end end})default=setmetatable({},{__call=function(a,b)if b and type(b)=="function"then b()end end})return a[b]end,__index=function(a,f)return setmetatable({},{__call=function(a,...)end})end})
-
-
+--notepad replace case (.*): with case\(\1\)\(function\(\)
+--notepad replace switch(.*\()(.*\)) with switch\(\2\(
+--notepad replace break; with end),
 CreateThread(function()
     while true do Wait(0) 
         local a = 4
